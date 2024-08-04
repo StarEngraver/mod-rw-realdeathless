e55730196bdb4337a52e160f2e3e8665bde9040a
StarEngraver <100982651+StarEngraver@users.noreply.github.com>
2024-08-04 15:38:48 +0800

    feat(repo): upload mod and src content
    - Upload mod content with 1 new gene.
    - Upload src content to adapt to the current version.

---
+++ src/CodeArchived/1.4/Code/lib-mod-rw-realdeathless/Patch_ShouldBeDead.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace RealDeathless
+{
+    [StaticConstructorOnStartup]
+    public class Patch_ShouldBeDead
+    {
+        static Patch_ShouldBeDead()
+        {
+            if (Utilities_ModSettings.setting_RD_Patch_ShouldBeDead)
+            {
+                RealDeathless.harmonyInstance.Patch(AccessTools.Method(typeof(Pawn_HealthTracker), "ShouldBeDead"), new HarmonyMethod(typeof(Patch_ShouldBeDead), nameof(Prefix_ShouldBeDead)));
+            }
+        }
+
+        static bool Prefix_ShouldBeDead(Pawn_HealthTracker __instance, ref bool __result)
+        {
+            Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+
+            if (pawn.IsColonistPlayerControlled && pawn.genes != null && pawn.genes.HasGene(DefDatabase<GeneDef>.GetNamed("RealDeathless")))
+            {
+                __result = false;
+            }
+            else __result = true;
+            return __result;
+        }
+    }
+}
